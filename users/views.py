03f3c21a51010358e6dca3abc8516b77e4667ff1
kshuhambgs123 <kshubhambgs123@gmail.com>
2022-01-02 17:29:33 +0530

    initial commmit

---
+++ users/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
+from .forms import UserUpdateForm, ProfileUpdateForm
+
+def register(request):
+
+    if request.method == 'POST': # This is a POST Request
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')  # Grab the username that is submitted for now
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')
+
+    else:    # This is not a POST Request. We will just create a form
+
+        form = UserRegisterForm()
+
+    return render(request, 'users/UserRegister.html', {'form':form})
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':   
+        u_form = UserUpdateForm(request.POST, instance=request.user)  
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)  
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your profile has been updated!')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = { 
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, 'users/Userprofile.html', context)
